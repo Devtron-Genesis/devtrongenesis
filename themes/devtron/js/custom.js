309718464141311c7072aee8e5fc752191926d96
KashifAli5 <kashif.ali@devtrongenesis.com>
2019-03-28 18:34:59 +0500

    front page spaces issues and some extra classes

---
+++ themes/devtron/js/custom.js
@@ -0,0 +1,8 @@
+(function($){
+  $(document).ready(function() {
+
+  	$('.contact_us_main').parents('.fh5co-section-contact').parent().find('.field__item').addClass('hi');
+  	$('#outer-page .field--name-field-paragraph-reference :nth-child(3)').addClass('address_contact_form');
+
+}); /* End of the document.ready */
+})(jQuery);
