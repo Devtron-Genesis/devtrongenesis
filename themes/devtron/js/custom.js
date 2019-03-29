3285a18b82097195d1945325acc7b5200072ffca
KashifAli5 <kashif.ali@devtrongenesis.com>
2019-03-29 18:57:27 +0500

    added current language class to body

--- themes/devtron/js/custom.js
+++ themes/devtron/js/custom.js
@@ -3,6 +3,14 @@
 
   	$('.contact_us_main').parents('.fh5co-section-contact').parent().find('.field__item').addClass('hi');
   	$('#outer-page .field--name-field-paragraph-reference :nth-child(3)').addClass('address_contact_form');
+  	// $('body').hasClass('en').addClass('language_en');
+
+  	if ($('#block-languageswitcher ul .en').hasClass('is-active')) {
+  		$('body').addClass('language-en');
+  	}
+  	else {
+  		$('body').addClass('language-nb');
+  	}
 
 }); /* End of the document.ready */
 })(jQuery);
