91793756669b24a04647066b06876ec941b8dd91
KashifAli5 <kashif.ali@devtrongenesis.com>
2019-03-29 19:06:53 +0500

    hide the current langauge option

--- themes/devtron/js/custom.js
+++ themes/devtron/js/custom.js
@@ -3,14 +3,18 @@
 
   	$('.contact_us_main').parents('.fh5co-section-contact').parent().find('.field__item').addClass('hi');
   	$('#outer-page .field--name-field-paragraph-reference :nth-child(3)').addClass('address_contact_form');
-  	// $('body').hasClass('en').addClass('language_en');
-
+  	
+  	// to add current langauge into body as class
   	if ($('#block-languageswitcher ul .en').hasClass('is-active')) {
   		$('body').addClass('language-en');
   	}
   	else {
   		$('body').addClass('language-nb');
   	}
+
+  	// to remove the current language option from the page.
+  	$('.language-en li.en.is-active').hide();
+  	$('.language-nb li.nb.is-active').hide();
 
 }); /* End of the document.ready */
 })(jQuery);
